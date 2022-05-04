226b5a5a11eaaecf8d9dc23ff3498eacb9bcf8fc
Sakna Hewa Galamulage <sakna.hewagalamulage@eskmapping.com.au>
2022-05-04 19:28:01 +1000

    initial commit

---
+++ public/main.js
@@ -0,0 +1,29 @@
+async function init() {
+
+    let rustApp = null 
+
+    try {
+        rustApp = await import('../pkg')
+    } catch (e) {
+        console.error(e);
+        return
+    }
+
+    console.log(rustApp);
+    
+    const input = document.getElementById('upload')
+    const fileReader = new FileReader()
+
+    fileReader.onload = () => {
+        const base64 = fileReader.result.replace(
+            /^data:image\/(png|jpeg|jpg);base64,/,''
+        )
+        console.log(base64, input.files[0]);
+    }
+
+    input.addEventListener('change', () => {
+        fileReader.readAsDataURL(input.files[0])
+    })
+}
+
+init()
